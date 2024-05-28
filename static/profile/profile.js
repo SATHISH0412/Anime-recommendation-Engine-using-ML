777c2a6267acf181281561b4ea85c100dd85d31d
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2024-05-28 21:24:15 +0000

    add wishlist

--- static/profile/profile.js
+++ static/profile/profile.js
@@ -10,25 +10,56 @@ document.addEventListener("DOMContentLoaded", function () {
   var profileIcon = document.getElementById("profileIcon");
   document.getElementById("Loading").innerHTML = "Fetching Data....";
 
+  // Function to create a table row with user data
+  function createTableRow(table, key, value) {
+    const row = table.insertRow();
+    const attributeCell = row.insertCell(0);
+    const valueCell = row.insertCell(1);
+
+    attributeCell.textContent = key;
+    valueCell.textContent =
+      key === "password" ? `*****${value.slice(4)}` : value;
+  }
+
   // Function to populate the profile table
   function populateProfileTable(userData) {
-    document.getElementById("Loading").innerHTML = "";
+    const loadingElement = document.getElementById("Loading");
+    loadingElement.innerHTML = "";
 
-    for (var key in userData) {
-      if (
-        userData.hasOwnProperty(key) &&
-        key !== "uid" &&
-        key !== "UserLoggedIn" &&
-        key !== "emailVerified"
-      ) {
-        var row = profileTable.insertRow();
-        var attributeCell = row.insertCell(0);
-        var valueCell = row.insertCell(1);
-        var value =
-          key === "password" ? `*****${userData[key].slice(4)}` : userData[key];
+    for (let key in userData) {
+      if (userData.hasOwnProperty(key)) {
+        if (
+          key !== "uid" &&
+          key !== "UserLoggedIn" &&
+          key !== "emailVerified" &&
+          key !== "wishlist"
+        ) {
+          createTableRow(profileTable, key, userData[key]);
+        }
 
-        attributeCell.textContent = key;
-        valueCell.textContent = value;
+        if (key === "wishlist") {
+          populateWishlist(userData[key]);
+        } else {
+          document.getElementById("wishlist-list").innerHTML = "";
+        }
+      }
+    }
+  }
+
+  // Function to populate the wishlist
+  function populateWishlist(wishlist) {
+    const wishlistContainer = document.getElementById("wishlist-list");
+    wishlistContainer.innerHTML = ""; // Clear any existing items
+
+    for (let item in wishlist) {
+      if (wishlist.hasOwnProperty(item)) {
+        const wishlistItem = document.createElement("div");
+        wishlistItem.classList.add("wishlist-item");
+        wishlistItem.innerHTML = `
+        <img src="${wishlist[item].imageURL}" alt="${wishlist[item].animeName}">
+        <h3>${wishlist[item].animeName}</h3>
+      `;
+        wishlistContainer.appendChild(wishlistItem);
       }
     }
   }
