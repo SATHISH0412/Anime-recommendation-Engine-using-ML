eaad910ffa5dbc738fb0d02f8e9c91b7f6c0d126
Muhamed ibrahim <92036859+Mohammadibbu@users.noreply.github.com>
2024-06-04 15:11:41 +0000

    Revert "css"
    This reverts commit c65bc0066e2504a3fe47cfe8fd60bfda01581199.

--- static/script2.js
+++ static/script2.js
@@ -4,35 +4,10 @@ import {
   connectDB,
   set,
 } from "./firebaseConnection/firebaseDBconn.js";
-// Notify function to show success or error messages
-var notifyTimeout;
+import { NotifyUser } from "./loginpage.js";
 
-function NotifyUser(ErrorType, message, duration) {
-  var errorMessage = document.getElementById("NotifyUser");
-
-  // Clear any existing timeout
-  clearTimeout(notifyTimeout);
-
-  errorMessage.innerHTML = "";
-
-  // Set message type and content
-  if (ErrorType === "success") {
-    errorMessage.classList.add("successMessage");
-    errorMessage.innerHTML = `<i class="fa fa-check" style="font-size:20px" aria-hidden="true"></i> ${message}`;
-  } else {
-    errorMessage.classList.add("errorMessage");
-    errorMessage.innerHTML = `<i class="fa fa-exclamation-circle" style="font-size:20px" aria-hidden="true"></i> ${message}`;
-  }
-
-  // Show the message and hide it after the duration
-  errorMessage.classList.remove("none");
-  notifyTimeout = setTimeout(() => {
-    errorMessage.classList.add("none");
-    errorMessage.classList.remove("errorMessage", "successMessage");
-    errorMessage.innerHTML = "";
-  }, duration);
-}
 const but = document.getElementById("submit");
+
 
 function SendFeedback(Desid, name, email, desc) {
   set(ref(connectDB, "loggers/" + Desid), {
